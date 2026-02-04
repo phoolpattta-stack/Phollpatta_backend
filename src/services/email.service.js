2b71c751c59a0226c63f855486529bffba67f968
phoolpattta-stack <phoolpattta@gmail.com>
2026-02-05 02:47:41 +0530

    Update email.service.js

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -30,8 +30,8 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT) || 465,
-  secure: true, // true for port 465
+  port: Number(process.env.MAIL_PORT) || 587,
+  secure: false, // true for port 465
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
